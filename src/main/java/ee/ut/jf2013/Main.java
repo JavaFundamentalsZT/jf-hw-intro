7ddf58a970249a3994e0aad46d4e128d37d25c20
Toomas Romer <toomas@zeroturnaround.com>
2013-01-31 11:44:28 +0200

    Changed 2012 to 2013

---
+++ src/main/java/ee/ut/jf2013/Main.java
@@ -0,0 +1,11 @@
+package ee.ut.jf2013;
+
+import ee.ut.jf2013.hw1.HW;
+
+public class Main {
+  public static void main(String[] args) {
+    HW hw = new HW();
+    
+    System.out.println(hw.square(4));
+  }
+}
