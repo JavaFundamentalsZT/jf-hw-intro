2814d2674959bc6bc8b465f8f0cce6cf0af22fbf
Margus Pala <margus.pala@gmail.com>
2014-11-03 10:22:54 +0200

    Homework 9 boilerplate added

---
+++ src/main/java/ee/ut/jf2014/homework9/FibCached.java
@@ -0,0 +1,48 @@
+package ee.ut.jf2014.homework9;
+
+import java.math.BigInteger;
+
+public class FibCached {
+
+    static class MyCache {
+        //TODO implment this class
+    }
+    
+    static MyCache cache = new MyCache();
+    // this uses too much memory
+    // static Map<Long, BigInteger> cache = new HashMap<Long, BigInteger>();
+
+    public static BigInteger fib(long n) {
+        if (n == 0 || n == 1) {
+            return BigInteger.ONE;
+        }
+        BigInteger cached = cache.get(n);
+        if (cached != null) {
+            return cached;
+        }
+
+        BigInteger result = fib(n - 1).add(fib(n - 2));
+        cache.put(n, result);
+        return result;
+    }
+
+    public void calculate() {
+        int i = 0;
+        BigInteger result = null;
+        for (i = 0; i < 100000; i++) {
+            // long start = System.currentTimeMillis();
+            result = fib(i);
+            // long end = System.currentTimeMillis();
+            // System.out.println(i + " " + result + " " + (end - start));
+        }
+        System.out.println("done");
+        // report statistics
+        System.out.println("putCount=" + cache.putCount()); // number of calls to cache put method
+        System.out.println("getCount=" + cache.getCount()); // number of calls to cache get method
+        System.out.println("hitCount=" + cache.hitCount()); // number of calls to cache get method that found a value from cache
+        System.out.println("missCount=" + (cache.getCount() - cache.hitCount())); // number of calls to cache get method that did not find a value from cache
+        System.out.println("hitRatio=" + ((double) cache.hitCount()) / cache.getCount());
+        System.out.println("collectedCount=" + cache.collectedCount()); // number of elements that were put to cache and collected by gc
+    }
+
+}
