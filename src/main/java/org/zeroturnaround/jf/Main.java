81213aa3d70c68989b88760bc53ba4e454847df7
Sven Laanela <sven.laanela@gmail.com>
2017-01-26 14:43:15 +0200

    restored Main, skip tests by default, enable tests with -Ptest

---
+++ src/main/java/org/zeroturnaround/jf/Main.java
@@ -0,0 +1,10 @@
+package org.zeroturnaround.jf;
+
+import org.zeroturnaround.jf.homework1.Homework;
+
+public class Main {
+  public static void main(String[] args) {
+    Homework homework = new Homework();
+    System.out.println(homework.square(4));
+  }
+}
--- src/test/java/org/zeroturnaround/jf/homework1/HomeworkTest.java
+++ src/test/java/org/zeroturnaround/jf/homework1/HomeworkTest.java
@@ -13,27 +13,8 @@ public class HomeworkTest {
   }
 
   @Test
-  public void testReverseNull() {
-    Homework homework = new Homework();
-    assertEquals(null, homework.reverse(null));
-  }
-
-  @Test
   public void testGetCommonSuffix() {
     Homework homework = new Homework();
     assertEquals("JavaFundamentals", homework.getCommonSuffix("AmazingJavaFundamentals", "ILoveJavaFundamentals"));
   }
-
-  @Test
-  public void testGetCommonSuffixNoSuffix() {
-    Homework homework = new Homework();
-    assertEquals("", homework.getCommonSuffix("JavaFundamentalsIsAwesome", "ILoveJavaFundamentals"));
-  }
-
-  @Test
-  public void getGetCommonSuffixWithNullArgument() {
-    Homework homework = new Homework();
-    assertEquals("", homework.getCommonSuffix(null, "ILoveJavaFundamentals"));
-  }
-
 }
